b7b9897273393ff3c0209dc98999ee5dfe20d527
password-kim <72890183+password-kim@users.noreply.github.com>
2021-08-02 15:57:37 +0900

    What is Atomic
    Use atomic to data.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,43 +1,48 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-
 #include <thread>
+#include <atomic>
 
-void HelloThread()
+// atomic : All or Nothing
+atomic<int32> sum = 0; // 공유데이터를 atomic으로 만든다.
+
+void Add()
 {
-    cout << "Hello Thread" << endl;
+    for (int32 i = 0; i < 1000000; ++i)
+    {
+        sum.fetch_add(1);
+    }
 }
 
-void HelloThread_2(int32 num)
+void Sub()
 {
-    cout << num << endl;
+    for (int32 i = 0; i < 1000000; ++i)
+    {
+        sum.fetch_add(-1);
+    }
 }
 
 int main()
 {
+    Add();
+    Sub();
+    cout << sum << endl; // 1번 결과
 
-#pragma region ThreadFunc
-    //t.hardware_concurrency(); // CPU 코어 개수
-    //t.get_id(); // thread마다 id
-    //t.detach(); // std::thread 객체에서 실제 thread를 분리
-    //t.joinable(); // thread가 실제 객체를 관리하는 thread인지를 확인
-#pragma endregion
-    vector<thread> v;
+    thread t1(Add);
+    thread t2(Sub);
+    t1.join();
+    t2.join();
+    cout << sum << endl; // 2번 결과
 
-    for (int32 i = 0; i < 10; ++i)
-    {
-        v.push_back(thread(HelloThread_2, i));
-    }
-
-    for (int32 i = 0; i < 10; ++i)
-    {
-        if (v[i].joinable())
-            v[i].join();
-    }
-
-    cout << "Hello Main" << endl;
-
+    // 1번 결과와 2번 결과가 같다면 atomic이 제대로 이뤄진것.
+    /* 
+    atomic이 제대로 이뤄지지 않으면 
+    공유데이터를 두 쓰레드에서 꼬이게 사용되므로 
+    제대로된 결과값이 안나옴.
+    하지만 atomic은 동작이 매우 느리기 때문에
+    모든 데이터를 atomic화 하는건 매우 비효율적.
+    */
 
     return 0;
 }
