81a5979cfcb53b5e84d467f6b6ee29f582432ab9
password-kim <72890183+password-kim@users.noreply.github.com>
2021-09-14 22:55:14 +0900

    Local Thread Storage.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -8,51 +8,36 @@
 #include <Windows.h>
 #include <future>
 
-atomic<bool> ready;
-int32 value;
+// LTS (Local Thread Storage)
+// => thread마다 갖고 있는 각각의 storage.
 
+thread_local int32 LThreadID = 0; // 각각의 thread가 데이터를 가져가서 사용가능.
 
-void Producer()
+void ThreadMain(int32 threadid)
 {
-	value = 10;
-	ready.store(true, memory_order::memory_order_seq_cst);
+	LThreadID = threadid;
+
+	while (true)
+	{
+		cout << "Hi! I'm Thread " << LThreadID << endl;
+		this_thread::sleep_for(1s);
+	}
 }
-
-void Consumer()
-{
-	while (ready.load(memory_order::memory_order_seq_cst) == false)
-		;
-
-	cout << value << endl;
-}
-
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
+	vector<thread> threads;
 
-	// Memory Model (정책)
-	// 1) Sequentially Consistent (seq_cst)
-	// 2) Acquire-Release (acquire, release)
-	// 3) Relaxed (relaxed)
-
-	// 1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-	// => 가시성 문제 해결! 코드 재배치 바로 해결!
-
-	// 2) acquire-release
-	// => 딱 중간!
-	// => release명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는 것을 방지
-	// => 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면, release 이전의 명령들이 acquire하는 순간에 관찰 가능 (가시성 보장)
-
-	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적x)
-	// => 너무나도 자유롭다!
-	// => 코드 재배치도 멋대로 가능! 가시성 해결 NO!
-	// => 가장 기본 조건 (동일 객체에  대한 동일 관전 순서만 보장)
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
 	
+	for (thread& T : threads)
+	{
+		T.join();
+	}
+
 }
 
