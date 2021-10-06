1dfff34900cc902a39c9cda757373514886db9b8
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-06 13:01:49 +0900

    Make MemoryPool.
    Pooling similarly sized memory.

--- Server/ServerCore/CoreMacro.h
+++ Server/ServerCore/CoreMacro.h
@@ -18,8 +18,8 @@
 -----------------*/
 
 #ifdef _DEBUG
-#define Xalloc(size)		BaseAllocator::Alloc(size)
-#define Xrelease(ptr)		BaseAllocator::Release(ptr)
+#define Xalloc(size)		PoolAllocator::Alloc(size)
+#define Xrelease(ptr)		PoolAllocator::Release(ptr)
 #else
 #define Xalloc(size)		BaseAllocator::Alloc(size)
 #define Xrelease(ptr)		BaseAllocator::Release(ptr)
