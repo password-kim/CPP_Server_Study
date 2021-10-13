26377681977ad58c9929a20abacb2b49198b5b2c
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-13 14:49:08 +0900

    Basic of Non-Blocking Socket.

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -18,77 +18,81 @@ int main()
     if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         return 0;
 
-    // ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
-    // type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-    // protocol : 0
-    // return : descriptor
-    SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-    if (clientSocket == INVALID_SOCKET)
-    {
-        HandleError("Socket");
-        return 0;
-    }
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+		return 0;
 
-    SOCKADDR_IN serverAddr; // IPv4
-    ::memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-    serverAddr.sin_port = ::htons(7777); 
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
 
-    // Connected UDP
-    ::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
 
-    // ---------------------------
+	// Connect
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// 이미 연결된 상태라면 break
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+			// Error
+			break;
+		}
+	}
 
-    while (true)
-    {
-        for (int32 i = 0; i < 10; i++)
-        {
-            char sendBuffer[100] = "Hello World!";
+	cout << "Connected to Server!" << endl;
 
-            // 나의 IP 주소 + 포트 번호 설정
+	char sendBuffer[100] = "Hello World";
 
-            // Unconnected UDP
-            /*int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0,
-                (SOCKADDR*)&serverAddr, sizeof(serverAddr));*/
+	// Send
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// Error
+			break;
+		}
 
-            // Connected UDP
-            int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+		cout << "Send Data ! Len = " << sizeof(sendBuffer) << endl;
 
-            if (resultCode == SOCKET_ERROR)
-            {
-                HandleError("SendTo");
-                return 0;
-            }
+		// Recv
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
 
-            cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
-        }
+				// Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
 
-        SOCKADDR_IN recvAddr;
-        ::memset(&recvAddr, 0, sizeof(recvAddr));
-        int32 addrLen = sizeof(recvAddr);
+			cout << "Recv Data Len = " << recvLen << endl;
+			break;
+		}
 
-        char recvBuffer[1000];
+		this_thread::sleep_for(1s);
+	}
 
-        // Unconnected UDP
-        //int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
-        //	(SOCKADDR*)&recvAddr, &addrLen);
-
-        int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-        if (recvLen <= 0)
-        {
-            HandleError("RecvFrom");
-            return 0;
-        }
-
-        cout << "Recv Data! Data = " << recvBuffer << endl;
-        cout << "Recv Data! Len = " << recvLen << endl;
-
-        this_thread::sleep_for(1s);
-    }
-
-    // ---------------------------
 
     // 소켓 리소스 반환
     ::closesocket(clientSocket);
