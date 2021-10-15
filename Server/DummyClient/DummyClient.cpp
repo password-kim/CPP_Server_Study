1b8dbe86c590edaf5a4bf2f126a9151c389a3393
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-15 15:37:03 +0900

    Make WSAEventSelect Model.

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,22 +1,24 @@
 ﻿#include "pch.h"
 #include <iostream>
 
-#include <WinSock2.h>
+#include <winsock2.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
 void HandleError(const char* cause)
 {
-    int32 errCode = ::WSAGetLastError();
-    cout << cause << " ErrorCode : " << errCode << endl;
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
 }
 
 int main()
 {
-    WSAData wsaData;
-    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-        return 0;
+	this_thread::sleep_for(5s);
+
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
@@ -93,11 +95,9 @@ int main()
 		this_thread::sleep_for(1s);
 	}
 
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
 
-    // 소켓 리소스 반환
-    ::closesocket(clientSocket);
-
-    // 윈속 종료
-    ::WSACleanup();
+	// 윈속 종료
+	::WSACleanup();
 }
-
