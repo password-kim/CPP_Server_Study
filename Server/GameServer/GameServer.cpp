e2e9e3c8e9691607ac8bd0a6a0092df7a740701d
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-19 18:47:56 +0900

    Make Service Class.
    A class that acts as a server and a client.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,14 +9,24 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "SocketUtils.h"
-#include "Listener.h"
+#include "Service.h"
+#include "Session.h"
 
+// TEMP
+class GameSession : public Session
+{
+
+};
 
 int main()
 {
-	Listener listener;
-	listener.StartAccept(NetAddress(L"127.0.0.1", 7777));
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager ..etc
+		100);
+
+	ASSERT_CRASH(service->Start());
 
 	for (int32 i = 0; i < 5; i++)
 	{
@@ -24,7 +34,7 @@ int main()
 			{
 				while (true)
 				{
-					GIocpCore.Dispatch();
+					service->GetIocpCore()->Dispatch();
 				}
 			});
 	}
