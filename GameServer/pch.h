593ac6a6cf561a0546b704f3c3cd0477bd1de2fc
password-kim <72890183+password-kim@users.noreply.github.com>
2021-11-13 19:34:36 +0900

    Use Protobuf.

---
+++ GameServer/pch.h
@@ -0,0 +1,16 @@
+#pragma once
+// 여기에 미리 컴파일하려는 헤더 추가
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#ifdef _DEBUG
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#else
+#pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
+#endif
+
+
+#include "CorePch.h"
+
+
---
+++ ServerCore/CorePch.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Types.h"
+#include "CoreGlobal.h"
+#include "CoreTLS.h"
+#include "CoreMacro.h"
+#include "Container.h"
+
+
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include "Lock.h"
+#include "ObjectPool.h"
+#include "TypeCast.h"
+#include "Memory.h"
+#include "SendBuffer.h"
+#include "Session.h"
