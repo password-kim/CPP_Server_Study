8f4a9e5f8f16822381b3de18b73b3588236a1a51
password-kim <72890183+password-kim@users.noreply.github.com>
2021-12-18 21:59:56 +0900

    JobQueue#5.

--- ServerCore/CoreGlobal.cpp
+++ ServerCore/CoreGlobal.cpp
@@ -5,12 +5,14 @@
 #include "DeadLockProfiler.h"
 #include "SocketUtils.h"
 #include "SendBuffer.h"
+#include "GlobalQueue.h"
 
-ThreadManager*		GThreadManager = nullptr;
-Memory*				GMemory = nullptr;
-SendBufferManager*	GSendBufferManager = nullptr;
+ThreadManager* GThreadManager = nullptr;
+Memory* GMemory = nullptr;
+SendBufferManager* GSendBufferManager = nullptr;
+GlobalQueue* GGlobalQueue = nullptr;
 
-DeadLockProfiler*	GDeadLockProfiler = nullptr;
+DeadLockProfiler* GDeadLockProfiler = nullptr;
 
 class CoreGlobal
 {
@@ -20,6 +22,7 @@ public:
 		GThreadManager = new ThreadManager();
 		GMemory = new Memory();
 		GSendBufferManager = new SendBufferManager();
+		GGlobalQueue = new GlobalQueue();
 		GDeadLockProfiler = new DeadLockProfiler();
 		SocketUtils::Init();
 	}
@@ -29,6 +32,7 @@ public:
 		delete GThreadManager;
 		delete GMemory;
 		delete GSendBufferManager;
+		delete GGlobalQueue;
 		delete GDeadLockProfiler;
 		SocketUtils::Clear();
 	}
--- ServerCore/CoreGlobal.h
+++ ServerCore/CoreGlobal.h
@@ -1,8 +1,9 @@
 #pragma once
 
-extern class ThreadManager*		GThreadManager;
-extern class Memory*			GMemory;
+extern class ThreadManager* GThreadManager;
+extern class Memory* GMemory;
 extern class SendBufferManager* GSendBufferManager;
+extern class GlobalQueue* GGlobalQueue;
 
-extern class DeadLockProfiler*	GDeadLockProfiler;
+extern class DeadLockProfiler* GDeadLockProfiler;
 
