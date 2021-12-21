d0c264984e15598c8f5a6fc92b2124708f8e616b
password-kim <72890183+password-kim@users.noreply.github.com>
2021-12-21 13:06:31 +0900

    DB Connection.

--- ServerCore/CoreGlobal.cpp
+++ ServerCore/CoreGlobal.cpp
@@ -7,14 +7,16 @@
 #include "SendBuffer.h"
 #include "GlobalQueue.h"
 #include "JobTimer.h"
+#include "DBConnectionPool.h"
 
-ThreadManager* GThreadManager = nullptr;
-Memory* GMemory = nullptr;
-SendBufferManager* GSendBufferManager = nullptr;
-GlobalQueue* GGlobalQueue = nullptr;
-JobTimer* GJobTimer = nullptr;
+ThreadManager*		GThreadManager = nullptr;
+Memory*				GMemory = nullptr;
+SendBufferManager*	GSendBufferManager = nullptr;
+GlobalQueue*		GGlobalQueue = nullptr;
+JobTimer*			GJobTimer = nullptr;
 
-DeadLockProfiler* GDeadLockProfiler = nullptr;
+DeadLockProfiler*	GDeadLockProfiler = nullptr;
+DBConnectionPool*	GDBConnectionPool = nullptr;
 
 class CoreGlobal
 {
@@ -27,6 +29,7 @@ public:
 		GGlobalQueue = new GlobalQueue();
 		GJobTimer = new JobTimer();
 		GDeadLockProfiler = new DeadLockProfiler();
+		GDBConnectionPool = new DBConnectionPool();
 		SocketUtils::Init();
 	}
 
@@ -38,6 +41,7 @@ public:
 		delete GGlobalQueue;
 		delete GJobTimer;
 		delete GDeadLockProfiler;
+		delete GDBConnectionPool;
 		SocketUtils::Clear();
 	}
 } GCoreGlobal;
